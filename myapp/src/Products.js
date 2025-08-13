3307835cacc0dd38a7f994656e2e668a12a0ffc4
BhajanHP <bhajanhpgowda@gmail.com>
2025-08-13 12:18:02 +0530

    Changes

--- myapp/src/Products.js
+++ myapp/src/Products.js
@@ -1,14 +1,15 @@
-import img4 from './Images/body_wash.jpg';
+import img4 from './Images/body_wash.png';
 import img3 from './Images/lip_butter.jpg';
 import img7 from './Images/car_perfume.jpg';
-import img6 from './Images/foaming_face_wash.jpg';
+import img6 from './Images/foaming_face_wash.png';
 import img2 from './Images/lip_scrub.jpg';
-import img5 from './Images/oil.jpg';
+import img5 from './Images/oil.png';
 import img1 from './Images/scrub.jpg';
 import img8 from './Images/solid_perfume.jpg';
 import img9 from './Images/face_wash.jpg';
 import img10 from './Images/soap.jpg';
 import img11 from './Images/body_lotion.jpg';
+import img12 from './Images/Shampoo.png';
 
 import { Link } from 'react-router-dom';
 
@@ -26,7 +27,8 @@ function Products() {
         { img: img8, name: "Coffee Soild Perfume" },
         { img: img9, name: "Coffee Face Wash" },
         { img: img10, name: "Coffee Soap" },
-        { img: img11, name: "Body Lotion" }
+        { img: img11, name: "Body Lotion" },
+        { img: img12, name: "Coffee Shampoo" }
     ];
 
     return (
--- myapp/src/SoapDetails.js
+++ myapp/src/SoapDetails.js
@@ -2,6 +2,10 @@ import React from "react";
 import soapImg1 from "./Images/amazon_soap1.png";
 import soapImg2 from "./Images/amazon_soap2.png";
 import soapImg3 from "./Images/soap_details.png";
+import reviewImg1 from "./Images/review1.png"; 
+import reviewImg2 from "./Images/review2.png"; 
+import reviewImg3 from "./Images/review1_soap_img.jpeg"; 
+import reviewImg4 from "./Images/review2_soap_img.png"; 
 import "./App.css";
 
 function SoapDetails() {
@@ -9,17 +13,27 @@ function SoapDetails() {
     <div className="soap-details">
       <h2>DECO Coffee Soap</h2>
       <p>
-       Infused with rich coffee extract, this soap detans, brightens, and refreshes your skin with every wash.
-Fades tan & dullness for an even glow Antioxidants protect from damage and slow signs of aging
-Moisturizing oils keep skin soft and hydrated Soothes & energizes with a refreshing coffee aroma
-Natural. Nourishing. Glow-Boosting.Stock up now for skin that’s radiant, smooth, and ready to shine every day.
+        Infused with rich coffee extract, this soap detans, brightens, and refreshes your skin with every wash.
+        Fades tan & dullness for an even glow Antioxidants protect from damage and slow signs of aging
+        Moisturizing oils keep skin soft and hydrated Soothes & energizes with a refreshing coffee aroma
+        Natural. Nourishing. Glow-Boosting. Stock up now for skin that’s radiant, smooth, and ready to shine every day.
       </p>
 
+      {/* Soap Images */}
       <div className="soap-images">
         <img src={soapImg1} alt="Coffee Soap" className="soap-img" />
         <img src={soapImg2} alt="Coffee Soap Packaging" className="soap-img" />
         <img src={soapImg3} alt="Coffee Soap Packaging2" className="soap-img" />
       </div>
+{/* Review Section */}
+<h3 className="reviewsHeading">REVIEWS</h3>
+<div className="review-images">
+  <img src={reviewImg1} alt="Customer Review 1" className="review-img" />
+  <img src={reviewImg2} alt="Customer Review 2" className="review-img" />
+  <img src={reviewImg3} alt="Customer Review 1 Image" className="review-img small-review-img" />
+  <img src={reviewImg4} alt="Customer Review 2 Image" className="review-img small-review-img" />
+</div>
+
     </div>
   );
 }
